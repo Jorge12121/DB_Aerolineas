765bf1d3241a64bd2e2ef8edb7a194423a107b98
jorge muñoz <128510812+Jorge12121@users.noreply.github.com>
2025-09-05 23:08:55 -0400

    Update runStatements.sql

--- runStatements.sql
+++ runStatements.sql
@@ -3,6 +3,33 @@
 -- Descripción: Consultas estadísticas sobre vuelos y clientes
 -- ==========================================
 
+
+-- ==========================================
+-- 1. Lugares al que más viajan los chilenos por año (últimos 4 años)
+-- ==========================================
+SELECT 
+    EXTRACT(YEAR FROM v.fecha) AS anio,
+    v.destino,
+    COUNT(*) AS cantidad_viajes
+FROM cliente c
+JOIN cliente_comp cc ON c.id_cliente = cc.id_cliente
+JOIN vuelo v ON cc.id_vuelo = v.id_vuelo
+WHERE c.nacionalidad = 'Chile'
+  AND v.fecha >= (CURRENT_DATE - INTERVAL '4 years')
+GROUP BY anio, v.destino
+ORDER BY anio, cantidad_viajes DESC;
+-- ==========================================
+-- 2. Secciones de vuelos más compradas por argentinos
+-- ==========================================
+SELECT 
+    cc.seccion,
+    COUNT(*) AS cantidad_compras
+FROM cliente c
+JOIN cliente_comp cc ON c.id_cliente = cc.id_cliente
+WHERE c.nacionalidad = 'Argentina'
+GROUP BY cc.seccion
+ORDER BY cantidad_compras DESC;
+
 -- =======================
 -- 3. Lista mensual de países que más gastan en volar (últimos 4 años)
 -- =======================
